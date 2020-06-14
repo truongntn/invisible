00144204932c161644b536a0f3ca240e31f9ad9e
truongntn <truongntn@gmail.com>
2020-06-14 10:54:20 +0700

    Add files via upload
    Split input string and loop all cities

--- invisible.js
+++ invisible.js
@@ -4,31 +4,26 @@ var cityTimeZones = require("city-timezones");
 var moment = require("moment-timezone");
 var inputLocation = "./weather New York, 10005, Tokyo, São Paulo, Berlin";
 var arrLocation = inputLocation.split(",");
-var cityLookup = cityTimeZones.lookupViaCity(arrLocation[1].replace(/\s/g, ""));
-try {
-    function getUsers() {
-        var fetch = require("node-fetch");
-        return (fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityLookup[0]["city"] + "," + cityLookup[0]["iso2"] + "&appid=01ee8e16309fecb0722fb9931922dc4a")
-            .then(function (res) { return res.json(); })
-            .then(function (res) {
-            console.log(res);
-        }));
-    }
-    function getWeather(query) {
-        var that = this;
-        var endpoint = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=01ee8e16309fecb0722fb9931922dc4a";
-        return that.http
-            .get(endpoint) //, {search: searchParams})
-            .map(function (res) { return res.json().main; })
-            .subscribe(function (res) {
-            console.log(res);
-        });
-    }
-    console.log(getUsers());
-    console.log(cityLookup[0]["timezone"]);
-    console.log(moment().tz(cityLookup[0]["timezone"]).format());
+var numLocation = 0;
+var i = 0;
+numLocation = arrLocation.length;
+function getWeather(cityName, countryCode) {
+    var fetch = require("node-fetch");
+    return (fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=01ee8e16309fecb0722fb9931922dc4a")
+        .then(function (res) { return res.json(); })
+        .then(function (res) {
+        console.log(res);
+    }));
 }
-catch (e) {
-    var result = e.Message;
-    console.log("Not find the city.");
+for (i = 0; i < numLocation; i++) {
+    try {
+        var cityLookup = cityTimeZones.lookupViaCity(arrLocation[i].replace(/\s/g, ""));
+        console.log((i + 1) + ". " + cityLookup[0]["timezone"]);
+        console.log("Current time: " + moment().tz(cityLookup[0]["timezone"]).format());
+        getWeather(cityLookup[0]["city"], cityLookup[0]["iso2"]);
+    }
+    catch (e) {
+        var result = e.Message;
+        console.log((i + 1) + ". Not find the city '" + arrLocation[i] + "'.");
+    }
 }
--- invisible.ts
+++ invisible.ts
@@ -3,35 +3,29 @@ import * as moment from "moment-timezone";
 
 const inputLocation = "./weather New York, 10005, Tokyo, São Paulo, Berlin";
 let arrLocation: string[] = inputLocation.split(",");
-let cityLookup = cityTimeZones.lookupViaCity(arrLocation[1].replace(/\s/g, ""));
+let numLocation: number = 0;
+let i:number = 0;
+numLocation = arrLocation.length;
 
-try {
-    function getUsers() {
-        const fetch = require("node-fetch");
-        return (
-            fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityLookup[0]["city"] + "," + cityLookup[0]["iso2"] + "&appid=01ee8e16309fecb0722fb9931922dc4a")
-                .then((res) => res.json())
-                .then((res) => {
-                    console.log(res);
-                })
-        );
-    }
-
-    function getWeather(query) {
-        var that = this;
-        const endpoint =
-            "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=01ee8e16309fecb0722fb9931922dc4a";
-        return that.http
-            .get(endpoint)//, {search: searchParams})
-            .map(res => res.json().main)
-            .subscribe(res => {
+function getWeather(cityName, countryCode) {
+    const fetch = require("node-fetch");
+    return (
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&appid=01ee8e16309fecb0722fb9931922dc4a")
+            .then((res) => res.json())
+            .then((res) => {
                 console.log(res);
-            });
+            })
+    );
+}
+
+for (i = 0; i < numLocation; i++) {
+    try {
+        let cityLookup = cityTimeZones.lookupViaCity(arrLocation[i].replace(/\s/g, ""));
+        console.log((i + 1) + ". " + cityLookup[0]["timezone"]);
+        console.log("Current time: " + moment().tz(cityLookup[0]["timezone"]).format());
+        getWeather(cityLookup[0]["city"], cityLookup[0]["iso2"]);
+    } catch (e) {
+        let result = e.Message;
+        console.log((i +1) + ". Not find the city '" + arrLocation[i] + "'.");
     }
-    console.log(getUsers());
-    console.log(cityLookup[0]["timezone"]);
-    console.log(moment().tz(cityLookup[0]["timezone"]).format());
-} catch (e) {
-    let result = e.Message;
-    console.log("Not find the city.");
 }
